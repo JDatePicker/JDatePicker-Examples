1092ba3b9acc40e1fdd0c338881d108018d29b95
Juan Heyns <juanheyns@gmail.com>
2014-10-15 15:31:46 +0200

    Test project to use IntelliJ IDEA GUI builder.

---
+++ IDEA-Swing-Project/src/TestWidget.java
@@ -0,0 +1,7 @@
+import javax.swing.*;
+
+/**
+ * Created by jheyns on 2014/10/14.
+ */
+public class TestWidget extends JTextField {
+}
--- Vanilla-Swing/src/main/java/org/jdatepicker/examples/vanilla_swing/Main.java
+++
@@ -1,9 +0,0 @@
-package org.jdatepicker.examples.vanilla_swing;
-
-public class Main {
-
-    public static void main(String[] args) {
-        System.out.println("Main method");
-    }
-
-}
